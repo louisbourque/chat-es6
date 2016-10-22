5324c991d83e695a8ec0b8049213a7144efdd7c8
Louis Bourque <louisrbourque@gmail.com>
2016-10-22 16:11:20 -0600

    remove unused code

--- src/js/app.js
+++ src/js/app.js
@@ -182,5 +182,3 @@ function hasClass(el, className) {
 	    if (el.classList) el.classList.remove(className);
 	    else el.className = el.className.replace(new RegExp('\\b'+ className+'\\b', 'g'), '');
 	}
-
-//window.showChat();
